9d6e1812cb757b46a108e80705ba71a97a45b516
lamnguyen <lam18061999@gmail.com>
2023-07-03 23:52:32 +0700

    update domain

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa4347130e1bf44e8aa465949e2a4307-1697724047.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
